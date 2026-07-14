7554cd1569a3f4de5c14ebd2fe4684f15b0c73ab
Monomee <165058731+Monomee@users.noreply.github.com>
2026-07-14 21:20:55 +0700

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public bool SearchMatrix(int[][] matrix, int target) {
+        int cols = matrix[0].Length;
+        int rows = matrix.Length;
+        
+        int low = 0;
+        int high = rows * cols - 1;
+
+
+        while (low <= high){
+            int mid = high - (high - low) / 2;
+
+            int r = mid / cols;
+            int l = mid % cols;
+
+            int value = matrix[r][l];
+
+            if (value == target) return true;
+
+            if (value < target) low = mid + 1;
+            else high = mid - 1;
+        }
+
+        return false;
+    }
+
+}
